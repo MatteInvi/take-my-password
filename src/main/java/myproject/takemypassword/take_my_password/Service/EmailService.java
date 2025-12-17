9c6fe3da62640396a3325c3e917c773aef9a938d
MatteInvi <matteoinvidia@gmail.com>
2025-12-17 19:18:12 +0100

    Add Resend email service integration and configuration

--- src/main/java/myproject/takemypassword/take_my_password/Service/EmailService.java
+++ src/main/java/myproject/takemypassword/take_my_password/Service/EmailService.java
@@ -1,11 +1,14 @@
 package myproject.takemypassword.take_my_password.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import jakarta.mail.internet.MimeMessage;
+import com.resend.Resend;
+import com.resend.services.emails.model.CreateEmailOptions;
+import com.resend.services.emails.model.CreateEmailResponse;
+
 import myproject.takemypassword.take_my_password.model.AuthToken;
 import myproject.takemypassword.take_my_password.model.ResetToken;
 import myproject.takemypassword.take_my_password.model.User;
@@ -16,75 +19,92 @@ public class EmailService {
     @Autowired
     JavaMailSender mailSender;
 
+    private final Resend resend;
+
+    public EmailService(@Value("${resend.mail.api.key}") String apiKey) {
+        this.resend = new Resend(apiKey);
+    }
+
     // Email di invio token di verifica
     public void sendVerificationEmail(User user, AuthToken token) {
         // Implementazione dell'invio email
+
+        String confirmationUrl = "https://take-my-password-react-app.onrender.com/confirm?token="
+                + token.getToken();
+
+        String html = String.format(
+                """
+                        <html>
+
+                        <body style="background-color: lightgray; padding:20px;">
+                            <main style="margin:0 auto; width: 50%%; position: relative; top: 50%%;">
+                                <h1 style="">Conferma la tua registrazione</h1>
+                                <p>Clicca sul seguente link per confermare la registrazione a Take My Password</p>
+                                <a style="background-color: orange; text-align: center; color: black; font-size: x-large; border-radius: 10px; padding: 5px; text-decoration: none; display: block; width: 30%%;"
+                                    href="%s">Conferma</a>
+                            </main>
+                        </body>
+
+                        </html>
+
+                                    """,
+                confirmationUrl);
+        CreateEmailOptions params = CreateEmailOptions.builder()
+                .from("TakeMyPassword <takemypassword@matteoinvidia.it>")
+                .to(user.getEmail())
+                .subject("Conferma indirizzo email")
+                .html(html.toString())
+                .build();
+
         try {
-            MimeMessage message = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
-            String confirmationUrl = "https://take-my-password-react-app.onrender.com/confirm?token=" + token.getToken();
+            CreateEmailResponse data = resend.emails().send(params);
+            System.out.println(data.getId());
 
-            helper.setTo(user.getEmail());
-            helper.setSubject("Conferma la tua email");
-            String html = String.format(
-                    """
-                            <html>
-
-                            <body style="background-color: lightgray; padding:20px;">
-                                <main style="margin:0 auto; width: 50%%; position: relative; top: 50%%;">
-                                    <h1 style="">Conferma la tua registrazione</h1>
-                                    <p>Clicca sul seguente link per confermare la registrazione a Take My Password</p>
-                                    <a style="background-color: orange; text-align: center; color: black; font-size: x-large; border-radius: 10px; padding: 5px; text-decoration: none; display: block; width: 30%%;"
-                                        href="%s">Conferma</a>
-                                </main>
-                            </body>
-
-                            </html>
-
-                                        """,
-                    confirmationUrl);
-
-            helper.setText(html, true);
-
-            mailSender.send(message);
         } catch (Exception e) {
             System.err.println(e);
         }
+
     }
 
     // Email di reset password
     public void sendPasswordResetEmail(User user, ResetToken resetToken) {
+
         // Implementazione dell'invio email
+        String resetUrl = "https://take-my-password-react-app.onrender.com/reset-password/confirm?token="
+                + resetToken.getToken();
+
+        String html = String.format(
+                """
+                        <html>
+
+                        <body style="background-color: lightgray; padding:20px;">
+                            <main style="margin:0 auto; width: 50%%; position: relative; top: 50%%;">
+                                <h1 style="">Reset della tua password</h1>
+                                <p>Clicca sul seguente link per resettare la tua password</p>
+                                <a style="background-color: orange; text-align: center; color: black; font-size: x-large; border-radius: 10px; padding: 5px; text-decoration: none; display: block; width: 30%%;"
+                                    href="%s">Reset Password</a>
+                            </main>
+                        </body>
+
+                        </html>
+
+                                    """,
+                resetUrl);
+
+        CreateEmailOptions params = CreateEmailOptions.builder()
+                .from("TakeMyPassword <takemypassword@matteoinvidia.it>")
+                .to(user.getEmail())
+                .subject("Reset della tua password")
+                .html(html.toString())
+                .build();
+
         try {
-            MimeMessage message = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
-            String resetUrl = "https://take-my-password-react-app.onrender.com/reset-password/confirm?token=" + resetToken.getToken();
+            CreateEmailResponse data = resend.emails().send(params);
+            System.out.println(data.getId());
 
-            helper.setTo(user.getEmail());
-            helper.setSubject("Reset della tua password");
-            String html = String.format(
-                    """
-                            <html>
-
-                            <body style="background-color: lightgray; padding:20px;">
-                                <main style="margin:0 auto; width: 50%%; position: relative; top: 50%%;">
-                                    <h1 style="">Reset della tua password</h1>
-                                    <p>Clicca sul seguente link per resettare la tua password</p>
-                                    <a style="background-color: orange; text-align: center; color: black; font-size: x-large; border-radius: 10px; padding: 5px; text-decoration: none; display: block; width: 30%%;"
-                                        href="%s">Reset Password</a>
-                                </main>
-                            </body>
-
-                            </html>
-
-                                        """,
-                    resetUrl);
-
-            helper.setText(html, true);
-
-            mailSender.send(message);
         } catch (Exception e) {
             System.err.println(e);
         }
+
     }
 }
